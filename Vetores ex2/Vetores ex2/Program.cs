a64aacc0ee763d1e2a1e9908cce0a2f483f6e82f
Guilherme Marcio <guilhermemarcio281@gmail.com>
2025-11-08 22:44:20 -0300

    feat: adicione 2 exercicios de vetores

---
+++ Vetores ex2/Vetores ex2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores_ex2
+{
+    internal class Program
+    {
+        static double[] RetornaResult (double[] a)
+        {
+            double[] b = new double[3];
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = a[i] * a[i];
+
+            }
+
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            double[] a = new double[3];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º número.");
+                a[i] = double.Parse(Console.ReadLine());
+            }
+
+            double[] b = RetornaResult(a);
+
+            /*
+             Console.WriteLine($"{String.Join("/ ", c)}"); 
+            */
+
+            for (int i = 0;i < b.Length; i++)
+            {
+                Console.WriteLine($"O quadrado do {i + 1}º número = {b[i]} ");
+            }
+        }
+    }
+}
---
+++ Vetores ex3/Vetores ex3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores_ex3
+{
+    internal class Program
+    {
+        static double[] Combinador(double[] a, double[] b)
+        {
+            double[] combinacao = a.Concat(b).ToArray();
+
+            return combinacao;
+        }
+
+        static void Main(string[] args)
+        {
+            double[] a = new double[3];
+            double[] b = new double[3];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º valor do vetor A ");
+                a[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Digite o {i + 1}º valor do vetor B ");
+                b[i] = double.Parse(Console.ReadLine());
+
+            }
+            double[] c = Combinador(a, b);
+            string resultado = String.Join("/", c);
+
+            Console.WriteLine($"Vetor Combinado:[ {resultado} ]");
+        }
+    }
+}

935358fdf107daf2d39bee5ce4df659c896fc474
Guilherme Marcio <guilhermemarcio281@gmail.com>
2025-11-09 19:47:21 -0300

    feat: adiciona 2 exercicios de vetores

---
+++ Vetores ex4/Vetores ex4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores_ex4
+{
+    internal class Program
+    {
+        static double MediaTurma(double[] a)
+        {
+            int media = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] >= 6)
+                {
+                    media++;
+                }
+            }
+            return media;
+        }
+        static void Main(string[] args)
+        {
+            double[] a = new double[5];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"Digite a {i + 1}º nota: ");
+                a[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Existem {MediaTurma(a)} notas acima da média.");
+
+
+        }
+    }
+}
---
+++ Vetores ex5/Vetores ex5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores_ex5
+{
+    internal class Program
+    {
+        static double[] SomaouMult(double[] a)
+        {
+            double[] b = new double[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    b[i] = a[i] * 5;
+                }
+                else
+                {
+                    b[i] = a[i] + 5;
+                }
+            }
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            double[] a = new double[10];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º número:");
+                a[i] = double.Parse(Console.ReadLine());
+            }
+            
+            Console.Clear();
+            string vetora = String.Join("/", a);
+            string vetorb = String.Join("/", SomaouMult(a));
+            Console.WriteLine($"O conteúdo do vetor A é {vetora}");
+            Console.WriteLine($"O conteúdo do vetor B é {vetorb}");
+        }
+    }
+}
